389e6a43d658501da99238d541fae49c67bfd566
Mohammed_Akhtar01 <Mohammed_Akhtar01@PUNITP135360L.ad.infosys.com>
2018-09-01 16:40:04 +0530

    Created new Automation Project

---
+++ Udemy_SeleniumJenkins/src/testcases/TC_002.java
@@ -0,0 +1,29 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_002 {
+
+	WebDriver driver;
+	
+	@Test
+	public void testcase2() {
+		
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("email")).sendKeys("user1");
+		driver.findElement(By.id("pass")).sendKeys("pass1");
+		driver.findElement(By.xpath("//label[@id='loginbutton']/input")).click();
+		
+		driver.quit();
+	}
+	
+}
+
+
